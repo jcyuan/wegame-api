f1bcce4220552d88e447fd198490462abdafd73a
J.C <jcyuan.space@gmail.com>
2018-07-24 17:36:04 +0800

    1, fixed parameter error for createXXXButton, and parameter error for createVideo. 2, passed tslint test

--- datacontext/index.ts
+++ datacontext/index.ts
@@ -1,33 +1,31 @@
 wx.onMessage(data => {
-
     console.log("cmd in worker -> " + data.item);
 
-    if(data.item === "friend") {
+    switch (data.item) {
+        case "friend":
         wx.getFriendCloudStorage({
-            keyList:["selfOpenId"],
-            success: function(res){
+            keyList: ["selfOpenId"],
+            success: res => {
                 console.log("----------> in worker - getFriendCloudStorage: " + console.dir(res.data));
             }
-        })
-    }
-    else if(data.item === "user") {
+        });
+        break;
+    case "user":
         wx.getUserInfo({
-            openIdList:["selfOpenId"],
+            openIdList: ["selfOpenId"],
             success: res => {
                 console.log("----------> in worker - getUserInfo: " + res.data[0].nickName);
             }
         });
-    }
-    else if(data.item === "group") {
+        break;
+    case "group":
         wx.getGroupCloudStorage({
-            shareTicket:"",
-            keyList:["selfOpenId"],
-            success:function(res){
+            shareTicket: "",
+            keyList: ["selfOpenId"],
+            success: res => {
                 console.log("----------> in worker - getGroupCloudStorage: " + res.data);
             }
-        })
-        
+        });
+        break;
     }
-
 });
-
